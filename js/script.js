ce884cfb032a631c2dfdde05822850683bb7b580
frdnrdb <froden@gmail.com>
2017-07-22 02:00:46 +0200

    v0.3

--- js/script.js
+++ js/script.js
@@ -9,16 +9,18 @@ function bubb(config, callback) {
 
     if (!testconfig(config)) return;
 
-    let bubbs = [].slice.call( document.querySelectorAll('[data-bubb], [data-bubb-menu]') );
+    config.globals = config.globals || {};
+
+    let bubbs = Array.from( document.querySelectorAll('[data-bubb], [data-bubb-menu]') );
 
     if (!bubbs.length) return;
 
     bubbs.forEach(build);
 
-    function build(el) {
+    function build(infotip) {
 
-      let info = el.dataset.bubb,
-          menu = el.dataset.bubbMenu,
+      let info = infotip.dataset.bubb,
+          menu = infotip.dataset.bubbMenu,
           keys = menu ? menu.indexOf(',') > -1 ? menu.split(',') : [menu] : [info],
           html = '',
 
@@ -26,7 +28,8 @@ function bubb(config, callback) {
           option_color,
           option_callback,
           option_hover,
-          option_transition;
+          option_transition,
+          option_delay;
 
           keys.forEach( key => {
 
@@ -36,50 +39,51 @@ function bubb(config, callback) {
                   content = options ? current[0] : current,
                   trigger = (menu && callback) || (options && options.callback) ? ' data-bubb-value="'+trimmed+'"' : '';
 
-              html += '<div'+trigger+'>'+content+'</div>';
+              html += '<div' + trigger + '>' + content + '</div>';
 
-              if (options) {
-                option_callback = option_callback || options.callback;
-                option_hover = option_hover || options.hoverCallback;
-                option_background = option_background || options.background;
-                option_color = option_color || options.color,
-                option_transition = option_transition || options.transitionOff;
+              if (options || Object.keys(config.globals).length) {
+                option_callback = option_callback || options.callback || config.globals.callback;
+                option_hover = option_hover || options.hoverCallback || config.globals.hoverCallback;
+                option_background = option_background || options.background || config.globals.background;
+                option_color = option_color || options.color || config.globals.color;
+                option_transition = option_transition || options.transitionOff || config.globals.transitionOff;
+                option_delay = option_delay || options.delay || config.globals.delay;
               }
 
           });
 
-          let bubb = document.createElement('aside');
+          let aside = document.createElement('aside');
 
           if (option_background) {
-            bubb.style.background = option_background;
-            bubb.style.borderBottomColor = option_background;
+            aside.style.background = option_background;
+            aside.style.borderBottomColor = option_background;
           }
+          if (option_color) aside.style.color = option_color;
+          if (option_transition) infotip.classList.add('bubb-still');
+          if (option_delay) infotip.classList.add('bubb-delayed');
 
-          if (option_color) bubb.style.color = option_color;
+          if (menu) infotip.classList.add('bubb-menu'); // convenience class for styling
 
-          if (option_transition) el.classList.add('bubb-still');
+          infotip.classList.add('bubb');
 
-          bubb.innerHTML = html;
-
-          el.appendChild(bubb);
-          el.classList.add('bubb');
-          if (menu) el.classList.add('bubb-menu');
+          aside.innerHTML = html;
+          infotip.appendChild(aside);
 
           if ( !( (menu && callback) || option_callback ) ) return;
 
-          el.classList.add('bubb-interact'); // make the bubb hoverable
+          infotip.classList.add('bubb-interactive'); // make the bubb hoverable
 
-          el.addEventListener(option_hover ? 'mouseover' : 'click', e => {
+          infotip.addEventListener(option_hover ? 'mouseover' : 'click', e => {
 
-            let hover = option_callback && option_callback !== 'click',
-                bubbvalue = hover ? e.target.dataset.bubb : e.target.dataset.bubbValue || e.target.parentNode.dataset.bubbValue;
+            let hover = option_hover;
+            let bubbvalue = hover ? e.target.dataset.bubb : e.target.dataset.bubbValue || e.target.parentNode.dataset.bubbValue;
 
             if (bubbvalue) {
 
               let options = Array.isArray(config[bubbvalue]) ? config[bubbvalue][1] : {},
                   thiscallback = options.callback || callback;
 
-              thiscallback(bubbvalue, hover ? el.querySelector('aside') : e.target.dataset.bubbValue ? e.target : e.target.parentNode);
+              thiscallback(bubbvalue, hover ? aside : e.target.dataset.bubbValue ? e.target : e.target.parentNode);
 
             }
 
@@ -98,6 +102,8 @@ function testconfig(config) {
     let current = config[key],
         normal = typeof current === 'string',
         options = Array.isArray(current);
+
+    if (key === 'globals' && typeof current === 'object') return;
 
     if (!normal && !options) {
       console.log('bubb aborted: config value should be string or array');
@@ -122,7 +128,7 @@ function testconfig(config) {
 
 }
 
-if (typeof exports !== "undefined" && exports !== null) exports.bubb = bubb;
+if (typeof exports !== 'undefined' && exports !== null) exports.bubb = bubb;
 // todo! add support for amd/ requirejs
 else window.bubb = bubb;
 
